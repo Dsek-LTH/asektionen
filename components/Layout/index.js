c78ecd6dd6cd0e99d3e8216eb4d37821a60106c4
Ludvig Gierup <lgierup25@gmail.com>
2022-03-10 21:33:24 +0100

    Added footer as described in Figma

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,12 +1,14 @@
 import { Stack } from '@mui/material';
 import React from 'react';
 import Navbar from './Navbar';
+import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
     <Stack>
       <Navbar />
       <Stack component="main">{children}</Stack>
+      <Footer />
     </Stack>
   );
 };
