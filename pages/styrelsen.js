d263679d341cd07bee98cdecf9b1734a209ff331
Ludvig Gierup <lgierup25@gmail.com>
2022-03-10 14:46:01 +0100

    Updated styrelsen and boardmember, now looks close to figma specifications

--- pages/styrelsen.js
+++ pages/styrelsen.js
@@ -12,15 +12,18 @@ import BoardMember from '../components/BoardMember';
   flex-direction: column;
   max-width: 1350px;
   border-radius: 135px;
-  padding: 32px;
-  margin: 32px;
+  padding: 80px;
+  margin: 16px;
 `; 
 
+
+//Change Stack to Grid?
 export default function StyrelsenPage() {
   return(
-		<Stack alignItems="center">
+		<Stack alignItems="center"> 
 			<BackgroundPaper>
 				<Typography
+					marginBottom={8}
 					variant="h1"
 					fontSize={96}
 					fontWeight="bold"
@@ -29,9 +32,8 @@ export default function StyrelsenPage() {
 				>
 					STYRELSEN
 				</Typography>	
-				<Stack direction="row" justifyContent="flex-start" spacing="0" flexWrap="wrap">
+				<Stack direction="row" justifyContent="space-between" flexWrap="wrap">
 					{BOARD_MEMBERS.map((member) => <BoardMember {...member}/>	)}
-					
 				</Stack>
 			</BackgroundPaper>
 		</Stack>
